51d0a588872282f88c086222c612f995f196964a
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-01-26 15:39:26 -0500

    Added Example using Inheritance, overriding methods added and gitignore file.

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+
+    public void sleep() {
+        System.out.println("The animal is sleeping");
+    }
+
+    public void eat() {
+        System.out.println("The animal is eating");
+    }
+
+}
---
+++ src/MainAnimal.java
@@ -0,0 +1,14 @@
+public class MainAnimal {
+
+
+    public static void main(String[] args) {
+
+
+        Jaguar jaguar = new Jaguar(3, (float) 4.5);
+
+        jaguar.eat();
+        jaguar.sleep();
+
+
+    }
+}
