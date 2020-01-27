e87c97e862de386015a96ecd92d98ca68015c310
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-01-27 16:29:06 -0500

    Extending the functionality of the Builder including a conditional to enable more methods.

--- src/Builder/Main.java
+++ src/Builder/Main.java
@@ -4,13 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User user = User.Make("Emi", "Cas")
+//        User user = User.Make("Emi", "Cas")
+//                .setEmail("emi@emi.com")
+//                .setTelephone("XXX-XXX-XXXX")
+//                .setAddress("222 Av. 1")
+//                .Build();
+//
+//        System.out.println(user);
+
+        User emi = User.Make("Emi", "Cas")
+                .setAvailableContact(true)
                 .setEmail("emi@emi.com")
-                .setTelephone("XXX-XXX-XXXX")
-                .setAddress("222 Av. 1")
                 .Build();
 
-        System.out.println(user);
+        System.out.println(emi);
 
 
     }
--- src/Builder/User.java
+++ src/Builder/User.java
@@ -6,6 +6,8 @@ public class User {
     private String name;
     private String lastName;
 
+    private boolean availableContact; // Optional; True: email, tel, add activated. False: no available those before.
+
     // Optional Attributes
     private String email;
     private String telephone;
@@ -15,27 +17,29 @@ public class User {
     private User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
+
+        this.availableContact = false;
+
+        this.email = "";
+        this.telephone = "";
+        this.address = "";
+
+    }
+
+    public BuilderUser setAvailableContact(boolean availableContact) {
+
+        if (!availableContact) {
+            throw new IllegalArgumentException("It's not possible to assigning a false value to available contact");
+        }
+
+        this.availableContact = true;
+
+        return new BuilderUser(this);
     }
 
     // Step 2. Create a static method that return an Object of the same class. Usually named Make()
     public static User Make(String name, String lastName) {
         return new User(name, lastName);
-    }
-
-    // Step 3. Create Setter of the remain variables and return the same object (this).
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public User setTelephone(String telephone) {
-        this.telephone = telephone;
-        return this;
-    }
-
-    public User setAddress(String address) {
-        this.address = address;
-        return this;
     }
 
     // Step 4. Create a method that return the final object.
@@ -53,4 +57,38 @@ public class User {
                 ", address='" + address + '\'' +
                 '}';
     }
+
+    public static class BuilderUser{
+
+        // Create a field ofr user of the type User
+        private User user;
+
+        // Create constructor
+        public BuilderUser(User user) {
+            this.user = user;
+        }
+
+        // Move setters and change the return to BuilderUser
+        public BuilderUser setEmail(String email) {
+            user.email = email;
+            return this;
+        }
+
+        public BuilderUser setTelephone(String telephone) {
+            user.telephone = telephone;
+            return this;
+        }
+
+        public BuilderUser setAddress(String address) {
+            user.address = address;
+            return this;
+        }
+
+        // Add the Build method that returns the user attribute
+        public User Build() {
+            return user;
+        }
+    }
+
+
 }
